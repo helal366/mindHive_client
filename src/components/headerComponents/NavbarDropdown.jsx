f6421b4ab39723f6351a09bf1396b1accf89f9e3
Helal366 <helal56876@gmail.com>
2025-06-06 10:05:36 +0600

    motion installed

--- src/components/headerComponents/NavbarDropdown.jsx
+++ src/components/headerComponents/NavbarDropdown.jsx
@@ -19,7 +19,7 @@ const NavbarDropdown = () => {
       <div
         tabIndex={0}
         role="button"
-        className="btn btn-ghost btn-circle avatar"
+        className="btn btn-circle avatar"
       >
         <div className="w-10 rounded-full">
           <img
@@ -30,7 +30,7 @@ const NavbarDropdown = () => {
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        className="menu menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3 min-w-52 p-2 shadow-xl"
       >
         
         <li>
