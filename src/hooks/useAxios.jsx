09994e03d88b4feb5ff6a6c86bd4bd61b7a4f680
Helal366 <helal56876@gmail.com>
2025-06-08 09:39:37 +0600

    useeffect added in useaxios hook

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,16 +1,22 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
 
-const axiosInstance=axios.create({
-    baseURL:import.meta.env.VITE_API_URL
-})
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 const useAxios = () => {
-    const token=localStorage.getItem('token');
-    axiosInstance.interceptors.request.use(config=>{
-        config.headers.Authorization=`Bearer ${token}`
-        return config
-    })
-    return axiosInstance
+  useEffect(() => {
+    axiosInstance.interceptors.request.use((config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+  }, []);
+
+  return axiosInstance;
 };
 
 export default useAxios;
