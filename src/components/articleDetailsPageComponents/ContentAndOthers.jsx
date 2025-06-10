e57fe59fd9ed65b9502f434e92627da44f9ef9a2
Helal366 <helal56876@gmail.com>
2025-06-10 18:26:19 +0600

    like unlike functionality ok. takes a long time

--- src/components/articleDetailsPageComponents/ContentAndOthers.jsx
+++ src/components/articleDetailsPageComponents/ContentAndOthers.jsx
@@ -10,21 +10,21 @@ const ContentAndOthers = ({singleArticle}) => {
             <p className="font-semibold md:text-2xl sm:text-xl text-lg opacity-90 mb-2 sm:mb-4 md:mb-6 bg-base-100 px-2 sm:px-3 py-2 sm:py-3 rounded-xl">
               {title}
             </p>
-            <p className="font-medium text-base sm:text-lg md:text-xl mb-1 md:mb-2 opacity-80">
+            <p className="font-medium text-base sm:text-lg md:text-xl mb-1 md:mb-2 opacity-80 lg:mx-20 md:mx-12 sm:mx-4 inline-block bg-base-100 p-1 rounded-sm">
               <span>Article Category: </span>
               {category}
             </p>
-            <p className="opacity-60 text-xs sm:text-sm md:text-base text-justify mb-2 sm:mb-4">
+            <p className="opacity-60 text-xs sm:text-sm md:text-base text-justify mb-2 sm:mb-4 lg:px-20 md:px-12 sm:px-4">
               {content}
             </p>
-            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between">
-              <p className="opacity-60 text-xs mb-2">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between lg:px-20 md:px-12 sm:px-4">
+              <p className="opacity-60 text-xs mb-2 inline-block bg-base-100 p-1 rounded-sm">
                 <span>Published on: </span>
                 <span>{publishDate}</span>
               </p>
               <div>
                 {
-                    tags.map((tag,i)=><span key={i} className="badge badge-secondary mr-2 text-[10px]">{tag}</span>)
+                    tags?.map((tag,i)=><span key={i} className="badge badge-secondary mr-2 text-[10px]">{tag}</span>)
                 }
                 
               </div>
--- src/components/articleDetailsPageComponents/StickyTopBar.jsx
+++ src/components/articleDetailsPageComponents/StickyTopBar.jsx
@@ -1,45 +1,116 @@
 import React from "react";
-import { BiSolidCommentDetail, BiSolidLike } from "react-icons/bi";
+import { useState } from "react";
+import { BiSolidCommentDetail, BiSolidDislike, BiSolidLike } from "react-icons/bi";
+import Swal from "sweetalert2";
+import useAuth from "./../../hooks/useAuth";
+import useAxios from "./../../hooks/useAxios";
+import Loading from "./../Loading";
 
 const StickyTopBar = ({ singleArticle }) => {
-  const { authorEmail, authorName, authorPhoto } = singleArticle;
+  const axiosSucure = useAxios();
+  const { user } = useAuth();
+  const userEmail = user?.email;
+  const [loading, setLoading] = useState(false);
+  const { authorEmail, authorName, authorPhoto, _id, likes, likedUsers } =
+    singleArticle;
+  const [likeCount, setLikeCount] = useState(
+    likedUsers?.length>0 ? likedUsers.length : 0
+  );
+  const [likedUserEmails, setLikedUserEmails] = useState(
+    likedUsers?.length>0 ? likedUsers : []
+  );
+  
+  // handleLike function
+  const handleLike = () => {
+    if(!Array.isArray(likedUserEmails)){
+      setLikedUserEmails([]);
+      return
+    }
+    const emailExist = likedUserEmails?.includes(userEmail);
+    
+    let updatedLikes
+    let updatedUserEmails=[...likedUserEmails]
+    if (emailExist) {
+      updatedLikes = likeCount - 1;
+      updatedUserEmails = likedUserEmails?.filter(
+        (user) => user !== userEmail
+      );      
+    } else {
+      updatedLikes = likeCount + 1;
+      updatedUserEmails = [...likedUserEmails, userEmail];
+    }
+     const updatedDoc = {
+        likes: parseInt(updatedLikes),
+        likedUsers: updatedUserEmails,
+      };
+      setLikeCount(updatedLikes);
+      setLikedUserEmails(updatedUserEmails);
+    setLoading(true);
+    axiosSucure
+      .patch(`/article-like/${_id}`, updatedDoc)
+      .then((result) => {
+        console.log(result?.data);
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: "error",
+          title: "Failed to update",
+          text: err.message,
+          timer: 2000,
+        });
+        setLikeCount(emailExist?updatedLikes+1:updatedLikes-1);
+        setLikedUserEmails(emailExist?[...updatedUserEmails,userEmail]:updatedUserEmails.filter(email=>email!==userEmail))
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <>
-      <div className="z-20 mb-2 sm:mb-4 md:mb-8 rounded-lg bg-base-300 py-3 sm:py-4 px-1 sm:px-2 md:px-6 flex flex-wrap items-center justify-between sticky top-0 border border-gray-200">
-        <div className="flex gap-1 sm:gap-2 md:gap-4 flex-wrap items-center">
-          <div className="w-6 h-6 sm:w-8 sm:h-8  rounded-full p-1 ring-1 bg-white">
-            <img
-              className="w-full h-full object-cover rounded-full ring-1"
-              src={authorPhoto}
-              alt={authorName}
-            />
-          </div>
-          <p className="text-sm font-light opacity-90 hidden sm:block">{authorName}</p>
-          <p className="text-[10px] sm:text-xs font-light opacity-70 ">{authorEmail}</p>
-        </div>
-        <div className="flex gap-2 sm:gap-4 md:gap-8 items-center justify-end">
-          <div className="relative">
-            <div >
-                <BiSolidLike
-            className="cursor-pointer transition-transform duration-300 hover:scale-120 active:scale-90 text-neutral-600/60 p-1 border border-neutral-600 rounded-full"
-            size={24}
-          />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="z-20 mb-2 sm:mb-4 md:mb-8 rounded-lg bg-base-300 py-3 sm:py-4 px-1 sm:px-2 md:px-6 flex flex-wrap items-center justify-between sticky top-0 border border-gray-200">
+          <div className="flex gap-1 sm:gap-2 md:gap-4 flex-wrap items-center">
+            <div className="w-6 h-6 sm:w-8 sm:h-8  rounded-full p-1 ring-1 bg-white">
+              <img
+                className="w-full h-full object-cover rounded-full ring-1"
+                src={authorPhoto}
+                alt={authorName}
+              />
             </div>
-          <span className="absolute -top-3 right-1 text-[9px] sm:text-[10px] md:text-xs text-neutral-600 z-10 font-bold">
-            25
-          </span>
+            <p className="text-sm font-light opacity-90 hidden sm:block">
+              {authorName}
+            </p>
+            <p className="text-[10px] sm:text-xs font-light opacity-70 ">
+              {authorEmail}
+            </p>
           </div>
-          <div className="relative">
-            <div>
-                <BiSolidCommentDetail
-            className="cursor-pointer transition-transform duration-300 hover:scale-120 active:scale-90 text-neutral-600/60 p-1 border border-neutral-600 rounded-full"
-            size={24}
-          />
+          <div className="flex gap-2 sm:gap-4 md:gap-8 items-center justify-end">
+            <div className="relative">
+              <button
+                className="cursor-pointer transition-transform duration-300 hover:scale-120 active:scale-90 text-neutral-600/60 p-1 border border-neutral-600 rounded-full"
+                onClick={handleLike}
+              >
+                <BiSolidLike fill={`${likedUserEmails?.includes(userEmail)?'red':''}`} size={20} />
+                
+              </button>
+              <span className="absolute -top-3 right-1 text-[9px] sm:text-[10px] md:text-xs text-neutral-600 z-10 font-bold">
+                {likeCount}
+              </span>
             </div>
-          <span className="absolute -top-3 right-1 text-[9px] sm:text-[10px] md:text-xs text-neutral-600 z-10 font-bold">40</span>
+            <div className="relative">
+              <button className="cursor-pointer transition-transform duration-300 hover:scale-120 active:scale-90 text-neutral-600/60 p-1 border border-neutral-600 rounded-full">
+                <BiSolidCommentDetail size={20} />
+              </button>
+              <span className="absolute -top-3 right-1 text-[9px] sm:text-[10px] md:text-xs text-neutral-600 z-10 font-bold">
+                40
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
