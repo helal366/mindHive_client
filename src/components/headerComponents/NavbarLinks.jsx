94c477eae00972b78edfdb86bc5a360af7fdc02b
Helal366 <helal56876@gmail.com>
2025-06-15 14:34:37 +0600

    revised

--- src/components/headerComponents/NavbarLinks.jsx
+++ src/components/headerComponents/NavbarLinks.jsx
@@ -13,7 +13,6 @@ const NavbarLinks = () => {
   const navigate = useNavigate();
   const { baseURL } = useAuth();
   const [categories, setCategories] = useState([]);
-  const [openDropdown, setOpenDropdown] = useState(false);
   useEffect(() => {
     axios
       .get(`${baseURL}/categories`)
@@ -35,7 +34,7 @@ const NavbarLinks = () => {
       <NavCenterLinksButton to="/" label="Home" />
 
       <li className=" group relative">
-        <div className="flex items-center mr-1">
+        <NavLink to="/all-articles">
           <motion.button
             whileHover={{
               scale: 1.1,
@@ -43,34 +42,26 @@ const NavbarLinks = () => {
             }}
             whileTap={{ scale: 0.95 }}
             transition={{ duration: 0.3, ease: easeInOut }}
-            onClick={() => {
-              setOpenDropdown(false)
-              navigate("/all-articles")}}
             className="btn text-base-content bg-base-100 group relative"
           >
             <span className="text-cyan-600">All Articles</span>
           </motion.button>
-          <button 
-          className="cursor-pointer"
-          onClick={()=>setOpenDropdown(!openDropdown)}>
-            <FaChevronDown className={` transition-transform duration-300 ${openDropdown?'rotate-180':''}`} />
-          </button>
-        </div>
-        {/* dropdown */}
-        {
-          openDropdown && <ul className={`absolute bg-[#f1f4f7] shadow-xl z-50 mt-12 transition-all transform duration-400 ease-in-out rounded-xl min-w-52 p-5  ${openDropdown?'opacity-100 translate-0':'opacity-0 -translate-y-10'}`}>
-          {distinctCategories.map((category, i) => (
-            <li
-              key={i}
-              className="px-4 py-2 text-gray-900 bg-gray-300 hover:bg-gray-200 hover:font-semibold border-b border-teal-600 cursor-pointer rounded-xl mb-1"
-              // onClick={()=>navigate(`all-articles?category=${(category)}`)}
-              onClick={() => navigate(`category-articles/${category}`)}
-            >
-              {category}
-            </li>
-          ))}
-        </ul>
-        }
+        </NavLink>
+        {/* dropdown */}      
+          <ul
+            className={`absolute bg-[#f1f4f7] shadow-xl z-50 mt-16 transition-all duration-400 ease-in-out rounded-xl min-w-52 p-5 opacity-0 group-hover:opacity-100`}
+          >
+            {distinctCategories.map((category, i) => (
+              <li
+                key={i}
+                className="px-4 py-2 text-gray-900 bg-gray-300 hover:bg-gray-200 hover:font-semibold border-b border-teal-600 cursor-pointer rounded-xl mb-1"
+                // onClick={()=>navigate(`all-articles?category=${(category)}`)}
+                onClick={() => navigate(`category-articles/${category}`)}
+              >
+                {category}
+              </li>
+            ))}
+          </ul>
         
       </li>
       <NavCenterLinksButton to="/my-articles" label="My Articles" />
